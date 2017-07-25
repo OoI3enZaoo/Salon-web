5bbdc17398c233000768d45f5d79251aa2f4889f
OoI3enZaoO <Teerapath-Ben@hotmail.com>
2017-07-26 02:27:23 +0700

    d

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,6 +28,7 @@ module.exports = {
         { src : 'https://www.amcharts.com/lib/3/themes/light.js'},
         {src : 'https://www.amcharts.com/lib/3/pie.js'},
         {src : 'https://www.amcharts.com/lib/3/xy.js'}
+
     ],
     meta: [
       { charset: 'utf-8' },
@@ -37,7 +38,8 @@ module.exports = {
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' },
-      { rel: 'stylesheet', type: 'text/css', href: 'https://www.amcharts.com/lib/3/plugins/export/export.css' },
+      { rel: 'stylesheet', type: 'text/css', href: 'https://www.amcharts.com/lib/3/plugins/export/export.css' }
+
     ]
   },
   /*
--- pages/index.vue
+++ pages/index.vue
@@ -3,23 +3,29 @@
   <v-layout>
 
     <v-flex xs12 sm6 offset-sm3>
+
+
       <v-card>
 
         <v-card-media height="150px" class="primary lighten-4--text">
-          <h4 style="text-align:center; margin-top: 60px;" class="white--text">ยินดีต้อนรับสู่ระบบจัดการ Salon & Academy สำหรับผู้ดูแล</h4>
+          <h6 style="text-align:center; margin-top: 60px;" class="white--text">ยินดีต้อนรับสู่ระบบจัดการ Salon & Academy สำหรับผู้ดูแล</h6>
         </v-card-media>
         <v-card-title primary-title>
           <v-flex xs12 sm12>
             <v-text-field name="input-1" label="Email" id="testing" v-model="user.username"></v-text-field>
           </v-flex>
           <v-flex xs12 sm12>
-            <v-text-field name="input-1" label="Password" id="testing" v-model="user.password" @keyup.enter.native="Login"></v-text-field>
+            <v-text-field type ="password" name="input-1" label="Password" id="testing" v-model="user.password" @keyup.enter.native="Login"></v-text-field>
           </v-flex>
         </v-card-title>
         <v-card-actions>
           <v-btn primary style="width: 3000px" @click.native="Login">Login</v-btn>
         </v-card-actions>
       </v-card>
+
+      
+
+
     </v-flex>
   </v-layout>
 
@@ -28,16 +34,48 @@
 <script>
 
 
+import {db} from '../util/firebase'
 
+
+
+
+
+
+let adminRef = db.ref('admin')
+
+let series = db.ref('courses')
+series.on('value',(snapshot) =>{
+    snapshot.forEach((res)=>{
+      let data = res.val();
+
+      console.log("sold: " + Object.keys(data.sold).length);
+
+    })
+})
+console.log("series: " + series);
+adminRef.on('value', (snapshot)=>{
+  console.log("child_added: " + JSON.stringify(snapshot.val()));
+})
 export default {
- 
- 
+  firebase :{
+    adminRef
+  },
+
+
+
   data: () => ({
     user: {
       username: '',
       password: ''
-    }
+    },
+    defaultImage : ''
   }),
+  watch :{
+
+    // ref.on('child_added', function(childSnapshot, prevChildKey) {
+    //
+    // });
+  },
   mounted(){
 
       // console.log("cookie: "+document.cookie)
@@ -62,50 +100,37 @@ export default {
         this.$router.push('/home')
         console.log("INDEX.vue>> TRUE");
     }
-
-
   },
   methods: {
-    Login() {
-      if (this.user.username != '' && this.user.password != '') {
-        this.axios.get('https://salon-b177d.firebaseio.com/admin.json')
-          .then((response) => {
-            let status = false
-            let res = response.data
-            for (let data in res) {
-              console.log("data: " + JSON.stringify(res[data]));
-              if (this.user.username == res[data].username && this.user.password == res[data].password) {
-                status = true;
-                console.log("status = true");
-                break;
-              }
-            }
-            if (status == true) {
-              this.$store.commit('islogin',true)
-              localStorage.setItem('isLogin',true)
-              console.log("localStorage: " + localStorage.getItem("isLogin"));
-              this.$router.push('/home')
-            } else {
-              let obj = {
-                 title: 'ผิดพลาด',
-                 message: 'เข้าสู่ระบบไม่สำเร็จ กรุณาลองใหม่อีกครั้งค่ะ',
-                 type: 'error',
-                 onClose: this.onClose
-              }
-              //this.$refs.simplert.openSimplert(obj)
+    Login(){
+      console.log("Login2");
 
-            }
+      if(this.user.password != '' || this.user.username != ''){
+
+        adminRef.on('value' , (snapshot) => {
+          snapshot.forEach((data)=>{
+              let res = data.val()
+              console.log("username: " + res.username + " password: " + res.password);
+              if(this.user.username == res.username && this.user.password == res.password){
+                console.log("true");
+                  this.$store.commit('islogin',true)
+                  localStorage.setItem('isLogin',true)
+                  console.log("localStorage: " + localStorage.getItem("isLogin"));
+                  this.$router.push('/home')
+                return true
+              }else{
+                console.log("not found");
+              }
+
           })
-      } else {
-        let obj = {
-           title: 'ผิดพลาด',
-           message: 'เข้าสู่ระบบไม่สำเร็จ กรุณากรอกรหัสผ่านให้ครบถ้วนค่ะ',
-           type: 'error',
-           onClose: this.onClose
+
+          })
+        }else{
+          console.log("enter");
         }
-       // this.$refs.simplert.openSimplert(obj)
-      }
-    }//login
+    }
+
+
   }
 }
 </script>
